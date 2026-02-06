df8084fa0b5ef888ed6165475d9c33471f8f28c8
Nikhil Maharana <nikhilmaharana72@gmail.com>
2026-02-06 15:26:45 +0530

    Products

--- productpage/src/products.js
+++ productpage/src/products.js
@@ -1,8 +1,17 @@
 const products = [
-  { id: 1, name: "iPhone 15", category: "Mobile" },
+  { id: 1, name: "IPhone 15", category: "Mobile" },
   { id: 2, name: "Samsung Galaxy", category: "Mobile" },
   { id: 3, name: "MacBook Air", category: "Laptop" },
   { id: 4, name: "Dell XPS", category: "Laptop" },
+  { id: 5, name: "Moto G60", category: "Mobile"},
+  { id: 6, name: "Moto G32", category: "Mobile"},
+  { id: 7, name: "HP Victus", category: "Laptop"},
+  { id: 8, name: "Lenovo LOQ", category: "Laptop"},
+  { id: 9, name: "Samsung S23", category: "Mobile"},
+  { id: 10, name: "IPhone 16", category: "Mobile"},
+  { id: 11, name: "Lenovo Thinkpad", category: "Laptop"},
+  { id: 12, name: "Moto G82", category: "Mobile"},
+  { id: 13, name: "Moto G40 Fusion", category: "Mobile"},
 ];
 
 export default products;
